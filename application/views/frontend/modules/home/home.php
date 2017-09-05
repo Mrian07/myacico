357ccdce55da7fe0c171b268d4008bf37ee5bb6a
Lalang Gumirang <lalang@myacico.com>
2017-09-05 16:56:16 +0700

    update test

--- application/views/frontend/modules/home/home.php
+++ application/views/frontend/modules/home/home.php
@@ -19,3 +19,65 @@
 	<span class='myacico-title'>MYACICO FOR YOU</span>
 	<span class='myacico-title-small'>Our Official Store</span>
 </div>
+<div class='braind'>
+	<div class="row">
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/tpliNk.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/CX-small.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/vivo.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/SS-small.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/philips.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/peperone.png');?>' border='0' />
+		</div>
+	</div>	
+	<div class="row">
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/SD-small.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/oppo.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/HC-small.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/bantex.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/microsoft.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/kitchenaid.png');?>' border='0' />
+		</div>
+	</div>	
+	<div class="row">
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/lg.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/evercoss.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/electrolux.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/small-iphon-icon.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/sony.png');?>' border='0' />
+		</div>
+		<div class="col-sm-2">
+			<img u="image" width='100%' src='<?php echo base_url('images/braind/XO-small.png');?>' border='0' />
+		</div>
+	</div>	
+</div>
