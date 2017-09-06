ffd7a8d104d2cd04eca4b37587101b83527ffd58
nendy <nendi.ari@myacico.com>
2017-09-06 16:28:08 +0700

    auto domain config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/newmyacico';
+$config['base_url'] = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].str_replace('/index.php','',$_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
