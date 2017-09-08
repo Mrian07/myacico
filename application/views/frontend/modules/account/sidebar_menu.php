f37d0380f0f6a23341f88d7366b5dcd1368e64af
Lalang Gumirang <lalang@myacico.com>
2017-09-08 09:22:49 +0700

    update account

--- application/views/frontend/modules/account/sidebar_menu.php
+++ application/views/frontend/modules/account/sidebar_menu.php
@@ -1,7 +1,18 @@
-<div class='nav-account'><?php echo anchor('account/', 'Dashboard');?></div>
-<div class='nav-account'><?php echo anchor('account/informasiAkun/', 'Informasi Akun');?></div>
-<div class='nav-account'><?php echo anchor('account/bukuAlamat/', 'Buku Alamat');?></div>
-<div class='nav-account'><?php echo anchor('account/riwayatStatusPasaran/', 'Riwayat & Status Pesanan');?></div>
-<div class='nav-account'>Return Management Authority</div>
-<div class='nav-account'>Berlangganan Newsletter</div>
-<div class='nav-account'>Wishlist</div>
+<ul id="simple">
+    <li><?php echo anchor('account', 'Dashboard');?></li>
+    <li><?php echo anchor('account/informasiAkun', 'Informasi Akun');?></li>
+    <li><?php echo anchor('account/bukuAlamat', 'Buku Alamat');?></li>
+    <li><?php echo anchor('account/riwayatStatusPasaran', 'Riwayat & Status Pesanan');?></li>
+	<li><?php echo anchor('account/returnManagementAuthority', 'Return Management Authority');?></li>
+    <li><?php echo anchor('account/berlanggananNewsletter', 'Berlangganan Newsletter');?></li>
+    <li><?php echo anchor('account/Wishlist', 'Wishlist');?></li>
+</ul>
+
+<!--
+<div class='nav-account'><?php echo anchor('account', 'Dashboard');?></div>
+<div class='nav-account'><?php echo anchor('account/informasiAkun', 'Informasi Akun');?></div>
+<div class='nav-account'><?php echo anchor('account/bukuAlamat', 'Buku Alamat');?></div>
+<div class='nav-account'><?php echo anchor('account/riwayatStatusPasaran', 'Riwayat & Status Pesanan');?></div>
+<div class='nav-account'><?php echo anchor('account/returnManagementAuthority', 'Return Management Authority');?></div>
+<div class='nav-account'><?php echo anchor('account/berlanggananNewsletter', 'Berlangganan Newsletter');?></div>
+<div class='nav-account'><?php echo anchor('account/Wishlist', 'Wishlist');?></div>-->
