8b9888ce9499302e0eb44cd523d763acfd98b615
obbie <wahyu.suit@myacico.com>
2017-09-11 17:54:50 +0700

    Maps

--- application/views/frontend/modules/customer/contact/contact.php
+++ application/views/frontend/modules/customer/contact/contact.php
@@ -1,6 +1,6 @@
 
 
-<div id="map" style="width:1600px;height:400px;background:yellow"></div>
+
 
 <script>
 function myMap() {
@@ -13,7 +13,8 @@ var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
 </script>
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
+<iframe id="maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.649842298437!2d106.81277131436264!3d-6.17760599552759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f68061654a55%3A0xaafcf85e4760d02a!2sPT.+Myacico+Global+Indonesia!5e0!3m2!1sen!2sid!4v1496997240232" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+
 
 <div class="container-fluid">
 
