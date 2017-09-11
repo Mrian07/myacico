96e0f26488280d3accb3e75d9ae1cf49e024236b
obbie <wahyu.suit@myacico.com>
2017-09-11 17:18:21 +0700

    Personal Dan Login

--- application/views/frontend/modules/customer/sign_in/sign_in.php
+++ application/views/frontend/modules/customer/sign_in/sign_in.php
@@ -5,22 +5,22 @@
 			<h3 class='my-title-page'><i class="fa fa-dot-circle-o" aria-hidden="true"></i> LOGIN AKUN</h3>
 		</div>
 	  </div>
-	</div> 
-	
+	</div>
+
 	<div class="row">
 		<div class="col-sm-3">
-		
+
 		</div>
 	  <div class="col-sm-6">
   		<div class='border-create'>
 		    <form name="signup" method="post">
 		      <div class="form-group">
             <label>Email:</label>
-            <input type="email" name="email" class="form-control" required="" />
+            <input type="email" id="email" name="email" class="form-control"  />
 		      </div>
 		      <div class="form-group">
       			<label>Password:</label>
-      			<input type="password" name="password" class="form-control" required="" />
+      			<input type="password" id="password" name="password" class="form-control" />
     		  </div>
     		  <input type="submit" class="btn btn-primary" value="Kirim">
     			<div class="clearfix"></div>
@@ -31,14 +31,14 @@
             <div class="clearfix"></div>
             <br />
           </div>
-    		</form> 
+    		</form>
 		  </div>
 	  </div>
 	  <div class="col-sm-3">
-		
+
 		</div>
-	</div>  
-</div> 
+	</div>
+</div>
 <script type="text/javascript">
 var baseApiUrl = '<?php echo $baseApiUrl; ?>';
 
@@ -46,9 +46,25 @@ $(document).ready(function() {
 
   $('form').submit(function(e){
     e.preventDefault();
-
+		var email = $("#email").val();
+			var password = $("#password").val();
     var apiurl = baseApiUrl + '/aduser/login';
     var data = $(this).serialize();
+
+		if(email==''){
+			$.alert({
+				title: 'Alert!',
+				content: 'email tidak boleh kosong!',
+			})
+		}else
+			if(password==''){
+				$.alert({
+					title: 'Alert!',
+					content: 'password tidak boleh kosong!',
+				})
+			};
+
+
 
     // success handling
     var success = function(r){
