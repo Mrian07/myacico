ddc0e4479321407c77b8df4281dfcb43dd38fcbd
nendy <nendi.ari@myacico.com>
2017-09-18 13:23:39 +0700

    create business

--- application/views/frontend/modules/customer/create/business.php
+++ application/views/frontend/modules/customer/create/business.php
@@ -37,32 +37,34 @@
         </div>
         <div class="form-group">
           <label>NPWP*</label>
-          <input type="text" name="npwp" class="form-control mandatory" placeholder="" />
+          <input type="text" name="npwp" class="form-control mandatory"/>
         </div>
         <div class="form-group">
           <label>Alamat*</label>
           <input type="text" name="alamat1" class="form-control mandatory" />
-          <input type="text" name="alamat2" class="form-control mandatory" placeholder="" />
+          <input type="text" name="alamat2" class="form-control mandatory" />
         </div>
-        <div class="form-group">
-          <label>Negara*</label>
-          <select name="country" class="form-control mandatory" ></select>
-        </div>
-        <div class="form-group">
-          <label>Provinsi*</label>
-          <select name="province" class="form-control mandatory" ></select>
-        </div>
-        <div class="form-group">
-          <label>Kota*</label>
-          <input type="text" name="city" class="form-control mandatory" />
-        </div>
-        <div class="form-group">
+        <div class="form-group" style="display:none" id="ditric_box">
           <label>Kecamatan*</label>
-          <input type="text" name="kecamatan" class="form-control mandatory" />
+          <select name="kecamatan" id="ditric_sel" class="form-control mandatory"></select>
+        </div>
+        <div class="form-group" style="display:none" id="city_box">
+          <label>Kota*</label>
+          <select name="city" id="city_sel" class="form-control mandatory"></select>
         </div>
         <div class="form-group">
           <label>Kodepos*</label>
           <input type="text" name="zip" class="form-control mandatory" />
+        </div>
+        <div class="form-group" style="display: none;" id="region_box">
+          <label>Provinsi*</label>
+          <select name="province" id="region_sel" class="form-control mandatory"></select>
+        </div>
+        <div class="form-group">
+          <label>Negara*</label>
+          <select name="country" id="country_sel" class="form-control mandatory" disabled >
+            <option value="">--pilih--</option>
+          </select>
         </div>
         <div class="form-group">
           <label>Kata Sandi*</label>
@@ -94,12 +96,53 @@
 </div>
 <script type="text/javascript">
 var baseApiUrl = '<?php echo $baseApiUrl; ?>';
+$.ajaxSetup({
+  error: function(){
+    alert('service not available, please try again later');
+  },
+  timeout: 10000/*,
+  contentType: "application/json; charset=UTF-8"*/
+});
+
+function get_distric(){
+  $("#ditric_box").slideDown();
+  $("#ditric_sel").prop('disabled', true).html('<option value="">--pilih--</option>');
+  $.get("http://myacico.net:8080/myacico-service/api/cdistrict/getlistdistrictbycityid/"+$("#city_sel").val(), function(r){
+    r.forEach(function(o){
+      $("#ditric_sel").append("<option value='"+o.c_district_id+"'>"+o.name+"</option>");
+    });
+    $("#ditric_sel").prop('disabled', false);
+  }, "json" );
+}
+
+function get_city(){
+  $("#city_box").slideDown();
+  $("#city_sel").prop('disabled', true).html('<option value="">--pilih--</option>').unbind("change", get_distric);
+  $.get( "http://myacico.net:8080/myacico-service/api/ccity/getlistccitybyidregion/"+$("#region_sel").val(), function(r){
+    r.forEach(function(o){
+      $("#city_sel").append("<option value='"+o.c_city_id+"'>"+o.name+"</option>");
+    });
+    $("#city_sel").prop('disabled', false).change(get_distric);
+  }, "json" );
+}
+
+function get_region(){
+  $("#region_box").slideDown();
+  $("#region_sel").prop('disabled', true).html('<option value="">--pilih--</option>').unbind("change", get_city);
+  $.get( "http://myacico.net:8080/myacico-service/api/cregion/getlistcregionbyidccountry/"+$("#country_sel").val(), function(r){
+    r.forEach(function(o){
+      $("#region_sel").append("<option value='"+o.c_region_id+"'>"+o.name+"</option>");
+    });
+    $("#region_sel").prop('disabled', false).change(get_city);
+  }, "json" );
+}
 $(document).ready(function() {
   $("form").submit(function(e){
     e.preventDefault();
     var apiurl = baseApiUrl + '/aduser/add';
     var fl=document.signup;
     var data = $(this).serialize();
+    return alert(data);
 
     // success handling
 
@@ -126,7 +169,6 @@ $(document).ready(function() {
       alert(r.message);
     };
 
-
     if(form_ok==false) return false;
     if(fl.password.value!=$('#password2').val())alert('password not match!!!');
 
@@ -134,9 +176,15 @@ $(document).ready(function() {
       $('#spinner_img').show();
       $('#submit_btn').val('loading...').addClass('disabled');
       $.post( apiurl, data, success, "json" );
-
     }
-
   });
+
+  $.get("http://myacico.net:8080/myacico-service/api/ccountry/getlistccountry", function(r){
+    console.log(r);
+    r.forEach(function(o){
+      $("#country_sel").append("<option value='"+o.c_country_id+"'>"+o.name+"</option>");
+    });
+    $("#country_sel").prop('disabled', false).change(get_region);
+  }, "json" );
 });
 </script>
