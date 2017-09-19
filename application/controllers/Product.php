73365758a8ea0c6d9716a669ba289a6c1385b3e7
Lalang Gumirang <lalang@myacico.com>
2017-09-19 17:29:52 +0700

    slide product

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -44,6 +44,18 @@ class Product extends Web {
 		$this->load->view('frontend/footer',$this->data);
 	}
 	
+	public function detail()
+    {
+		//$this->load->view('frontend/test',$this->data);
+		$this->data['title_web'] = "Myacico.com - Home";
+		$this->load->view('frontend/header',$this->data);
+		$this->load->view('frontend/nav.php',$this->data);
+		// $this->load->view('frontend/slide_show.php',$this->data);
+		$this->load->view('frontend/modules/product/detail.php',$this->data);
+		
+		$this->load->view('frontend/footer',$this->data);
+	}
+	
 	public function product2()
     {
 		//$this->load->view('frontend/test',$this->data);
