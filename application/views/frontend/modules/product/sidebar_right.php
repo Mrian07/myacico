2faa299fa20da600784140edddae1cb6d80dbd95
Lalang Gumirang <lalang@myacico.com>
2017-09-20 17:14:30 +0700

    product item

--- application/views/frontend/modules/product/sidebar_right.php
+++ application/views/frontend/modules/product/sidebar_right.php
@@ -3,9 +3,20 @@
 	$cart = 1;
 	if($cart){
 	?>
-	<div class='title'><b>ITEMS(1)</b> <?php echo anchor('checkout/cart', 'Lihat Detal <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+	<div class='title'><b>ITEMS(2)</b> <?php echo anchor('checkout/cart', 'Lihat Detal <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
 ');?></div>
 	<table border='0' width='100%'>
+	<tr>
+		<td><img src="<?php echo base_url('images/demo/oppo-f3plus-gold.jpg'); ?>" border="0" height="50"></td>
+		<td>ASUS Zenfone Go [ZB500KL] [2/16GB] - Silver Blue</td>
+	</tr>
+	<tr>
+		<td colspan='2'>QUANTITY: 1</td>
+	</tr>
+	<tr>
+		<td colspan='2'><hr size='1'></td>
+	</tr>
+	
 	<tr>
 		<td><img src="<?php echo base_url('images/demo/oppo-f3plus-gold.jpg'); ?>" border="0" height="50"></td>
 		<td>ASUS Zenfone Go [ZB500KL] [2/16GB] - Silver Blue</td>
