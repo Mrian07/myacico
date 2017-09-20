73ab36860591cd887753c6bd9ab02aa142ef8a78
Lalang Gumirang <lalang@myacico.com>
2017-09-20 17:06:04 +0700

    product item

--- application/views/frontend/modules/product/product_items.php
+++ application/views/frontend/modules/product/product_items.php
@@ -1,3 +1,4 @@
+
 <div class="row my-sort-product">
 	<div class="col-sm-9">
 		Items 1 to 10 of 287 total 
@@ -21,8 +22,8 @@
 </div>
 <div class="row my-b-product">
 	<div class="col-sm-2"><img src="<?php echo base_url('images/demo/oppo-f3plus-gold.jpg'); ?>" border="0" height="100"></div>
-	<div class="col-sm-7"><div class='title-product'><?php echo anchor('product/detail', 'ASUS Zenfone Go [ZB500KL] [2/16GB] - Silver Blue');?></div>
-	Learn More</div>
+	<div class="col-sm-7"><h5 class='title-product'><?php echo anchor('product/detail', 'ASUS Zenfone Go [ZB500KL] [2/16GB] - Silver Blue');?></h5>
+	</div>
 	<div class="col-sm-3"><span class='product-price'>Rp 1.399.000</span><br><span class='product-stock'>Stock sisa 3<br>Dikirim hari ini/besok<br></span>
 		<div class="btnaddcart">
 		<?php echo anchor('customer/contact', '<button class="dropbtnaddcar">ADD TO CART</button>');?>
@@ -33,7 +34,7 @@
 <div class="row my-b-product">
 	<div class="col-sm-2"><img src="<?php echo base_url('images/demo/oppo-f3plus-gold.jpg'); ?>" border="0" height="100"></div>
 	<div class="col-sm-7"><h5 class='title-product'><?php echo anchor('product/detail', 'ASUS Zenfone Go [ZB500KL] [2/16GB] - Silver Blue');?></h5>
-	Learn More</div>
+	</div>
 	<div class="col-sm-3"><span class='product-price'>Rp 1.399.000</span><br><span class='product-stock'>Stock sisa 3<br>Dikirim hari ini/besok<br></span>
 		<div class="btnaddcart">
 		<?php echo anchor('customer/contact', '<button class="dropbtnaddcar">ADD TO CART</button>');?>
@@ -44,7 +45,7 @@
 <div class="row my-b-product">
 	<div class="col-sm-2"><img src="<?php echo base_url('images/demo/oppo-f3plus-gold.jpg'); ?>" border="0" height="100"></div>
 	<div class="col-sm-7"><h5 class='title-product'><?php echo anchor('product/detail', 'ASUS Zenfone Go [ZB500KL] [2/16GB] - Silver Blue');?></h5>
-	Learn More</div>
+	</div>
 	<div class="col-sm-3"><span class='product-price'>Rp 1.399.000</span><br><span class='product-stock'>Stock sisa 3<br>Dikirim hari ini/besok<br></span>
 		<div class="btnaddcart">
 		<?php echo anchor('customer/contact', '<button class="dropbtnaddcar">ADD TO CART</button>');?>
@@ -55,7 +56,7 @@
 <div class="row my-b-product">
 	<div class="col-sm-2"><img src="<?php echo base_url('images/demo/oppo-f3plus-gold.jpg'); ?>" border="0" height="100"></div>
 	<div class="col-sm-7"><h5 class='title-product'><?php echo anchor('product/detail', 'ASUS Zenfone Go [ZB500KL] [2/16GB] - Silver Blue');?></h5>
-	Learn More</div>
+	</div>
 	<div class="col-sm-3"><span class='product-price'>Rp 1.399.000</span><br><span class='product-stock'>Stock sisa 3<br>Dikirim hari ini/besok<br></span>
 		<div class="btnaddcart">
 		<?php echo anchor('customer/contact', '<button class="dropbtnaddcar">ADD TO CART</button>');?>
