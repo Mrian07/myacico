9658fe234902f0d7548c419fd2c60b80976907e2
Lalang Gumirang <lalang@myacico.com>
2017-09-22 08:43:21 +0700

    update

--- application/views/frontend/modules/product/detail.php
+++ application/views/frontend/modules/product/detail.php
@@ -1,1 +1,7 @@
-halaman detail
+<div class="row">
+  <div class="col-sm-4">.col-sm-4</div>
+  <div class="col-sm-6">
+	<?php $this->load->view('frontend/modules/product/detail',$this->data); ?>
+  </div>
+  <div class="col-sm-2">.col-sm-2</div>
+</div>
