53311d37d7039fc9b4c34ddf475e38ab218505e7
Lalang Gumirang <lalang@myacico.com>
2017-09-22 14:54:31 +0700

    detail product

--- application/views/frontend/modules/product/detail.php
+++ application/views/frontend/modules/product/detail.php
@@ -1,7 +1,114 @@
-<div class="row">
-  <div class="col-sm-4">.col-sm-4</div>
-  <div class="col-sm-6">
-	<?php $this->load->view('frontend/modules/product/detail',$this->data); ?>
-  </div>
-  <div class="col-sm-2">.col-sm-2</div>
+<script src="<?php echo base_url('assets/productpopup/js/vendor/jquery.js');?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/productpopup/dist/xzoom.min.js');?>"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/productpopup/css/xzoom.css');?>" media="all" /> 
+<link type="text/css" rel="stylesheet" media="all" href="<?php echo base_url('assets/productpopup/magnific-popup/css/magnific-popup.css');?>" />
+<script type="text/javascript" src="<?php echo base_url('assets/productpopup/magnific-popup/js/magnific-popup.js');?>"></script>
+
+<div class='my-detail-product'>
+	<div class="row line-detail-product">
+		<div class="col-sm-12">
+		Home / Kategori / List Produk / <b>SAMSUNG Caramel [E1272] - White </b>
+		</div>
+	</div>
+	
+	<div class="row">
+	  <div class="col-sm-4">
+	  
+		<div class="row">
+		  <div class="col-sm-5">
+			<div class="xzoom-container">
+			  <img class="xzoom5" id="xzoom-magnific" src="<?php echo base_url('images/gallery/preview/01_b_car.jpg');?>" xoriginal="<?php echo base_url('images/gallery/original/01_b_car.jpg');?>" />
+			  <div class="xzoom-thumbs">
+				<a href="<?php echo base_url('images/gallery/original/01_b_car.jpg');?>"><img class="xzoom-gallery5" width="80" src="<?php echo base_url('images/gallery/thumbs/01_b_car.jpg');?>"  xpreview="<?php echo base_url('images/gallery/preview/01_b_car.jpg');?>" title="The description goes here"></a>
+				<a href="<?php echo base_url('images/gallery/original/02_o_car.jpg');?>"><img class="xzoom-gallery5" width="80" src="<?php echo base_url('images/gallery/preview/02_o_car.jpg');?>" title="The description goes here"></a>
+				<a href="<?php echo base_url('images/gallery/original/03_r_car.jpg');?>"><img class="xzoom-gallery5" width="80" src="<?php echo base_url('images/gallery/preview/03_r_car.jpg');?>" title="The description goes here"></a>
+				<a href="<?php echo base_url('images/gallery/original/04_g_car.jpg');?>"><img class="xzoom-gallery5" width="80" src="<?php echo base_url('images/gallery/preview/04_g_car.jpg');?>" title="The description goes here"></a>
+			  </div>
+			</div>        
+		  </div>
+		  <div class="col-sm-7"></div>
+		</div>
+	  
+	  
+	  
+	  
+	  
+	  
+	  </div>
+	  <div class="col-sm-6 detail-product">
+		<h5 class='title-product'>SAMSUNG Caramel [E1272] - White</h5>
+		<div class="row">
+			<div class="col-sm-6">
+			by <span class='red-tx'>SAMSUNG</span><br>
+			MA #SAMSSMAR00002007279 | MFR #<br>
+			Rp 499,000 Rp 399,000<br>
+			<br><br>
+			Save Rp 100,000 (21%)
+			</div>
+			<div class="col-sm-6">Out of stock</div>
+		</div>
+		
+		
+	  </div>
+	  <div class="col-sm-2">.col-sm-2</div>
+	</div>
+	
+	
+	<ul class="nav nav-tabs">
+	  <li class="active"><a data-toggle="tab" href="#home">Rincian Barang</a></li>
+	  <li><a data-toggle="tab" href="#menu1">Deskripsi</a></li>
+	</ul>
+
+	<div class="tab-content">
+	  <div id="home" class="tab-pane fade in active">
+		<h3>HOME</h3>
+		<p>Some content.</p>
+	  </div>
+	  <div id="menu1" class="tab-pane fade">
+		<h3>Menu 1</h3>
+		<p>Some content in menu 1.</p>
+	  </div>
+	</div>
+	
+	
+	<!--
+	<div class="tab_container">
+		<input id="tab1" type="radio" name="tabs" checked>
+		<label for="tab1"><span>Rinican Barang</span></label>
+
+		<input id="tab2" type="radio" name="tabs">
+		<label for="tab2"><span>Deskripsi</span></label>
+
+		
+
+		<section id="content1" class="tab-content">
+			<h3>Headline 1</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			
+		</section>
+
+		<section id="content2" class="tab-content">
+			<h3>Headline 2</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+		  
+		</section>
+
+		
+	</div>
+	-->
+	
+	
+	
+	
+	
+	
 </div>
+
+<script src="<?php echo base_url('assets/productpopup/js/setup.js');?>"></script>
