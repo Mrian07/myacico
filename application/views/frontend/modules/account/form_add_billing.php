f4eed746601dd5ad66d568977fc1109e68000b93
samuel <samuel.adhirinaldi@live.com>
2017-10-04 11:49:50 +0700

    add alamat bill plus

--- application/views/frontend/modules/account/form_add_billing.php
+++ application/views/frontend/modules/account/form_add_billing.php
@@ -21,22 +21,17 @@
 			<div class="panel panel-default">
 				<div class="panel-body">
 				  <form name="signup" method="post">
-                                      <input type="hidden" id = "name" name="name" value="<?php echo $user->name; ?>"   />
-                                      <div class="form-group">
-					  <label><?php echo "Telepon 1:"; ?>*</label>
-                                      <input type="text" id = "phone" name="phone"   />
-                                      <label><?php echo "Telepon 2:"; ?>*</label>
-                                     <input type="text" id = "phone2" name="phone2" />
-                                      </div>
+                                      <input type="hidden" id = "name" name="name" value="<?php echo $user->name; ?>"   />                                     
 					<div class="form-group">
-
                                           <input type="hidden" id="isbillto" name="isbillto" value="N" />
                                           <input type="hidden" id="isshipto" name="isshipto" value="Y" />
                                           <input type="hidden" id="ispayfrom" name="ispayfrom" value="N" />
                                           <input type="hidden" id="isremitto" name="isremitto" value="N" />
-                                          <input type="hidden" id="address_name" name="address_name" value="kontrakan" />
-                                          <input type="hidden" id="address3" name="address3" value="kelurahan duri kosambi" />
-																					<input type="hidden" id="address4" name="address4" value="kelurahan duri 123" />
+                                          <div class="form-group">
+					  <label>Disimpan sebagai alamat (contoh: alamat rumah, alamat kantor dll.)*</label>
+					  <input type="text" id="address_name" name="address_name" class="form-control mandatory"/>
+					</div>
+                                          																<input type="hidden" id="address4" name="address4" value="kelurahan duri 123" />
 					</div>
 					<div class="form-group">
 					  <label><?php echo $lang_addres; ?>*</label>
@@ -143,7 +138,7 @@ $(document).ready(function() {
     var address2 = $("#address2").val();
     var address3 = $("#address3").val();
 		  var address4 = $("#address4").val();
-    var district_id = $("#ditric_sel").val();
+    var district_id = $("#ditric_id").val();
     //var kota = $("#city").val();
     //var propinsi = $("#province").val();
    // var negara = $("#country").val();
@@ -152,24 +147,26 @@ $(document).ready(function() {
     var isshipto = $("#isshipto").val();
     var ispayfrom = $("#ispayfrom").val();
     var isremitto = $("#isremitto").val();
-
+    var phone = $("#phone").val();
+    var phone2 = $("#phone2").val();
+    var name = $("#name").val();
     //var fl=document.signup;
 //    var data = $(this).serialize();
 //     return alert(data);die();
-data.name = name;
-data.phone = phone;
-data.phone2 = phone2;
-data.address_name = address_name;
-data.address1 = address1;
-data.address2 = address2;
-data.address3 = "address3";
-data.address4 = "address4";
-data.postal = postal;
-data.district_id = district_id;
-data.isbillto = 'Y';
-data.isshipto = 'Y';
-data.ispayfrom = 'Y';
-data.isremitto = 'Y';
+        data.name = name;
+        data.phone = phone;
+        data.phone2 = phone2;
+        data.address_name = address_name;
+        data.address1 = address1;
+        data.address2 = address2;
+        //data.address3 = "address3";
+        //data.address4 = "address4";
+        data.postal = postal;
+        data.district_id = district_id;
+        data.isbillto = 'Y';
+        data.isshipto = 'Y';
+        data.ispayfrom = 'Y';
+        data.isremitto = 'Y';
 		// data.name = name;
 		// data.phone = phone;
 		// data.phone2 = phone2;
@@ -215,7 +212,7 @@ data.isremitto = 'Y';
     };
     $('#spinner_img').show();
     $('#submit_btn').val('loading...').addClass('disabled');
-    //console.log('test'+data);
+    console.log('test_ '+token);
 
     $.ajax({ type:"POST", contentType: "application/json", data:JSON.stringify(data), dataType: "json", url: apiurl, success: success, timeout: 30000 });
 
