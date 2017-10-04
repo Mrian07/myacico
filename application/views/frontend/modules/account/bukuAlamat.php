cecc4527cafa8472af363509380d0aa732abfb70
unknown <lalang gumirang>
2017-10-04 18:45:39 +0700

    update

--- application/views/frontend/modules/account/bukuAlamat.php
+++ application/views/frontend/modules/account/bukuAlamat.php
@@ -52,7 +52,7 @@
 
 <script type="text/javascript">
 
-var base_url = '<?php echo base_url('account/editBukuAlamat') ?>';
+var link_url = '<?php echo base_url('account/editBukuAlamat') ?>';
 
 $(document).ready(function() {
 	var token = document.cookie.split('x-auth=')[1].split(';').shift();
@@ -74,7 +74,7 @@ $(document).ready(function() {
 		if(data.length == 0) return rumah.append('<p>Data tidak ditemukan</p>');
 		
 		 data.forEach(function(p){
-			 rumah.append('<tr><td>'+p.address_name+',  '+p.address1+' '+p.address2+' '+p.address3+' '+p.address3+' '+p.address4+' '+p.cityname+' '+p.postal+'</td><td><a href="'+base_url+'/'+p.id+'" class="btn btn-link" aria-label="Ubah"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td><td><a class="btn btn-link" id="hapus'+p.id+'" aria-label="Delete"><i class="fa fa-trash-o" aria-hidden="true"></i></a></td></tr>')
+			 rumah.append('<tr><td>'+p.address_name+',  '+p.address1+' '+p.address2+' '+p.address3+' '+p.address3+' '+p.address4+' '+p.cityname+' '+p.postal+'</td><td><a href="'+link_url+'/'+p.id+'" class="btn btn-link" aria-label="Ubah"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td><td><a class="btn btn-link" id="hapus'+p.id+'" aria-label="Delete"><i class="fa fa-trash-o" aria-hidden="true"></i></a></td></tr>')
 			 $("#hapus"+p.id).click(function(e){
 
 				var id = $("#id"+p.id).val();
