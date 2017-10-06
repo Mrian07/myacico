5295534795c65f9e6a368e9d5fb9b728745c7fa3
unknown <lalang gumirang>
2017-10-06 10:08:00 +0700

    update item

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -170,24 +170,24 @@ a{
 									<?php 
 									if($totalItems)
 									{ ?>
-								
+									<!--
 									<div class='my-cart-scroll'>
 		
-									<?php foreach ($this->cart->contents() as $items): ?>	
+									<?php// foreach ($this->cart->contents() as $items): ?>	
 									<div class="row my-cart">
-									  <div class="col-sm-3"><img src="<?php echo $items['image'];?>" style='margin-bottom:10px'></div>
-									  <div class="col-sm-7"><?php echo $items['name'];?><br>Rp.<?php echo $items['price'];?> (<?php echo $items['qty'];?>)Items<br></div>
+									  <div class="col-sm-3"><img src="<?php //echo $items['image'];?>" style='margin-bottom:10px'></div>
+									  <div class="col-sm-7"><?php// echo $items['name'];?><br>Rp.<?php // echo $items['price'];?> (<?php //echo $items['qty'];?>)Items<br><?php //echo $items['rowid'];?></div>
 									  <div class="col-sm-2">
-											<?php echo anchor('cart', '<i class="fa fa-trash" aria-hidden="true"></i>');?>
+											<?php// echo anchor('cart', '<i class="fa fa-trash" aria-hidden="true"></i>');?>
 										</div>
 									</div>
-									<?php endforeach; ?>
+									<?php// endforeach; ?>
 
 									</div>
 									<div>
-										<div class='my-total-cart'>TOTAL : <b><?php echo $this->cart->format_number($this->cart->total()); ?></b></div>
-										<?php echo anchor('cart', 'My Cart & Checkout', array('class'=>'btn btn-success my-btn-chekout'));?>
-									</div>
+										<div class='my-total-cart'>TOTAL : <b><?php// echo $this->cart->format_number($this->cart->total()); ?></b></div>
+										<?php // echo anchor('cart', 'My Cart & Checkout', array('class'=>'btn btn-success my-btn-chekout'));?>
+									</div>-->
 									<?php }else{ ?>
 									<center>Keranjang masih kosong</center>
 									<?php } ?>
