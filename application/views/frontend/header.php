f2ad38bfc1e7b8b33c3be17c9494cea1c9d71af4
unknown <lalang gumirang>
2017-10-16 09:14:37 +0700

    update

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -596,12 +596,12 @@ function dellItemCartToken(id,img,name,idcart){
 var base_url = '<?php echo base_url();?>';
 var base_path = base_url.split(location.host).pop();
 
-var login = 'http://	acc.myacico.co.id/myacico-account/account/masuk';
+var login = 'http://acc.myacico.co.id/myacico-account/account/masuk';
 // var login_base_url_api = 'acc.myacico.co.id/myacico-service';
 
 
 //
 //var api_base_url2 = <?php echo $baseApiUrl; ?>;
 //var api_base_url = 'http://myacico.net:8080/myacico-service/api';
-var api_base_url = <?php echo $baseApiUrl; ?>;
+var api_base_url = "<?php echo $baseApiUrl; ?>";
 </script>
