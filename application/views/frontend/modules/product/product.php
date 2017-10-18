0946a49e8eda91a887470f899322bf162b8a2150
unknown <lalang gumirang>
2017-10-18 17:12:56 +0700

    chakout

--- application/views/frontend/modules/product/product.php
+++ application/views/frontend/modules/product/product.php
@@ -9,8 +9,12 @@
 		</div>
 	</div>
 </div>
+<div class="container">
+	<?php $this->load->view('frontend/modules/product/product_items',$this->data); ?>
+</div>
+<!--
 <div class="row my-conten-product">
   <div class="col-sm-2"><?php //$this->load->view('frontend/modules/product/sidebar_left',$this->data); ?></div>
-  <div class="col-sm-10"><?php $this->load->view('frontend/modules/product/product_items',$this->data); ?></div>
-  <!-- <div class="col-sm-2"><?php // $this->load->view('frontend/modules/product/sidebar_right',$this->data); ?></div> -->
-</div>
+  <div class="col-sm-10"><?php //$this->load->view('frontend/modules/product/product_items',$this->data); ?></div>
+  <!-- <div class="col-sm-2"><?php // $this->load->view('frontend/modules/product/sidebar_right',$this->data); ?></div> 
+</div>-->
