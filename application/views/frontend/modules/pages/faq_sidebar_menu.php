ab7dbff680a69bc7660c1f56efd5f6335fb07656
unknown <lalang gumirang>
2017-10-20 09:38:02 +0700

    update

--- application/views/frontend/modules/pages/faq_sidebar_menu.php
+++ application/views/frontend/modules/pages/faq_sidebar_menu.php
@@ -1,4 +1,17 @@
 <div class="panel panel-primary">
-	<div class="panel-heading"><b>DATA PENGIRIM & PENERIMA</b></div>
-	<div class="panel-body">asdasdas</div>
+	<div class="panel-heading"><b>PERTANYAAN UMUM</b></div>
+	<div class="panel-body">
+		<ul>
+			<li>Home</li>
+		</ul>
+	</div>
+</div>		
+
+<div class="panel panel-primary">
+	<div class="panel-heading"><b>PERTANYAAN LAINNYA</b></div>
+	<div class="panel-body">
+		<ul>
+			<li>Home</li>
+		</ul>
+	</div>
 </div>		
