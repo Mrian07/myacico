c6aaa00bb2658f630f660eec664174de07519abe
unknown <lalang gumirang>
2017-10-26 17:16:11 +0700

    slider

--- application/views/frontend/slide_show.php
+++ application/views/frontend/slide_show.php
@@ -27,54 +27,62 @@ h1, h2, h3 {
     font-family: 'Crimson Text', sans-serif;
     font-weight: 100;
 }
-body {
+/*body {
     font: 15px 'Titillium Web', Arial, sans-serif;
     height: 100%;
     color: #aaa;
     margin: 0;
     padding: 0;
-}
+}*/
 
 .content {
     padding: 10px 15vw;
 }
+
+.padding-0{
+    padding-right:0;
+    padding-left:0;
+}
+
+.padding-1{
+    padding-right:0;
+    padding-left:0;
+}
 </style>
 
-<div style='background:#666666'>
+<div style='background:#dad8d8'>
 <div class="container">
   <div class="row">
-    <div class="col-sm-8">
+    <div class="col-sm-8 padding-0">
+      <!-- Slider 1 -->
+      <div class="slider" id="slider1">
+          <!-- Slides -->
 
+          <?php
+          $jmlslode=count($dathome['level_1']);
+          $n=1;
+          foreach($dathome['level_1'] as $key => $itemslide){
+            if($n=="$jmlslode"){
+              $active='active';
+            }else{
+              $active='';
+            }
+            ?>
+              <div style="background-image:url(<?php echo $itemslide['imageurl']; ?>)"></div>
+            <?php $n++; } ?>
 
-    <!-- Slider 1 -->
-    <div class="slider" id="slider1">
-        <!-- Slides -->
-
-        <?php
-        $jmlslode=count($dathome['level_1']);
-        $n=1;
-        foreach($dathome['level_1'] as $key => $itemslide){
-          if($n=="$jmlslode"){
-            $active='active';
-          }else{
-            $active='';
-          }
-          ?>
-            <div style="background-image:url(<?php echo $itemslide['imageurl']; ?>)"></div>
-          <?php $n++; } ?>
-
-        <!-- The Arrows -->
-        <i class="left" class="arrows" style="z-index:2; position:absolute;"><svg viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z"></path></svg></i>
-        <i class="right" class="arrows" style="z-index:2; position:absolute;"><svg viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" transform="translate(100, 100) rotate(180) "></path></svg></i>
-        <!-- Title Bar -->
-        <!-- <span class="titleBar">
-            <h1>This Slider has all default settings.</h1>
-        </span> -->
-    </div>
+          <!-- The Arrows -->
+          <i class="left" class="arrows" style="z-index:2; position:absolute;"><svg viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z"></path></svg></i>
+          <i class="right" class="arrows" style="z-index:2; position:absolute;"><svg viewBox="0 0 100 100"><path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" transform="translate(100, 100) rotate(180) "></path></svg></i>
+          <!-- Title Bar -->
+          <!-- <span class="titleBar">
+              <h1>This Slider has all default settings.</h1>
+          </span> -->
+      </div>
 
     </div>
-    <div class="col-sm-4">
-      <div class="row">
+    <div class="col-sm-4 padding-1">
+      <!-- <div class="row">
         <div class="col-sm-12" style="background:#f1f1f1; margin-left:-2px; padding:0px;">
 
         <img src="https://s1.postimg.org/4zblqmyx27/Artboard_3.jpg"  alt="Smiley face" width="50%" height="200">
@@ -82,18 +90,13 @@ body {
         </div>
 
 
-    </div>
+    </div> -->
 
-    <div class="row">
-      <div class="col-sm-12" style="margin-left:-2px; padding:0px;">
-        <img src="https://s1.postimg.org/1j99yjva1r/Artboard_5.jpg" alt="Smiley face" width="100%" height="139">
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-12" style="margin-left:-2px; padding:0px;">
-        <img src="https://s1.postimg.org/1j99yjva1r/Artboard_5.jpg" alt="Smiley face" width="100%" height="139">
-      </div>
-    </div>
+      <div style='margin-bottom:5px; margin-left:5px;'><img src="https://s1.postimg.org/1j99yjva1r/Artboard_5.jpg" alt="Smiley face" width="100%" height="160"></div>
+      <div style='margin-bottom:5px; margin-left:5px;'><img src="https://s1.postimg.org/1j99yjva1r/Artboard_5.jpg" alt="Smiley face" width="100%" height="160"></div>
+      <div style='margin-left:5px;'><img src="https://s1.postimg.org/1j99yjva1r/Artboard_5.jpg" alt="Smiley face" width="100%" height="170"></div>
+
+
   </div>
 </div>
 </div>
