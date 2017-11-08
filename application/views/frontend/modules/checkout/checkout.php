9a42abfcd4e82ad85207520f4bb9c00c003693bc
unknown <lalang gumirang>
2017-11-08 11:13:56 +0700

    title page

--- application/views/frontend/modules/checkout/checkout.php
+++ application/views/frontend/modules/checkout/checkout.php
@@ -33,15 +33,10 @@ a
 }
 </style>
 
-<div class='my-bg-title'>
-	<div class="container">
-		<div class="row">
-		  <div class="col-sm-12">
 
-			<i class="fa fa-angle-right" aria-hidden="true"></i> CHECKOUT
-
-		  </div>
-		</div>
+<div class="container">
+	<div class='my-bg-title'>
+		<i class="fa fa-angle-right" aria-hidden="true"></i> CHECKOUT
 	</div>
 </div>
 
