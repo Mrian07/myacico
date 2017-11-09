c62a0f31bafb8511c90dfb6ca3152ce8a23e282e
obbie <wahyu.suit@myacico.com>
2017-11-09 15:43:08 +0700

    test

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -37,7 +37,7 @@ class Home extends Web {
 		$this->data['dathome'] = json_decode($konten, true);
 
 		//$this->load->view('frontend/test',$this->data);
-		$this->data['title_web'] = "Myacico.com - Home";
+		$this->data['title_web'] = "myacico.co.id - PT. MyAcico Global Indonesia";
 		$this->load->view('frontend/header',$this->data);
 		$this->load->view('frontend/nav.php',$this->data);
 		$this->load->view('frontend/slide_show.php',$this->data);
