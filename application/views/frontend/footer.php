17a9e7f079bd68f13591c45814ae4cca816fcc7c
obbie <wahyu.suit@myacico.com>
2017-11-16 08:56:43 +0700

    test

--- application/views/frontend/footer.php
+++ application/views/frontend/footer.php
@@ -291,7 +291,13 @@ if(email==''){
 			});
       return false;
 		}
-
+		var x = document.forms["myForm"]["email"].value;
+		var atpos = x.indexOf("@");
+		var dotpos = x.lastIndexOf(".");
+		if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+				alert("Masukan Email Yang Benar");
+				return false;
+		}
 
 
 
