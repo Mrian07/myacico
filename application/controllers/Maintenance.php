331958d6712432b76f425c1439d6bfd233670a89
unknown <lalang gumirang>
2017-11-17 09:37:41 +0700

    update

--- application/controllers/Maintenance.php
+++ application/controllers/Maintenance.php
@@ -7,16 +7,19 @@ class Maintenance extends CI_Controller {
 			$url = "https://api.myacico.co.id/myacico-service/category";
       // $url = "http://192.168.0.109:8080/myacico-service/category";
   		$konten = file_get_contents($url);
+			if($konten){
   		$this->data['catsearch'] = json_decode($konten, true);
       $catsearch = json_decode($konten, true);
       if($catsearch==''){
 				$this->data['active_dashboard'] = "class='active'";
 				$this->data['title_web'] = "Myacico.com - Dashboard";
-				$this->load->view('frontend/maintenance',$this->data); 
+				$this->load->view('frontend/maintenance',$this->data);
       }else{
 				redirect('home');
 			}
-
+		}else{
+			redirect('home');
+		}
 
 	}
 }
