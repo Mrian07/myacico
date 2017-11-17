857b97bef3b6e7461699bba7f59c0341f14f70c4
obbie <wahyu.suit@myacico.com>
2017-11-17 14:25:03 +0700

    test

--- application/views/frontend/modules/customer/create/create.php
+++ application/views/frontend/modules/customer/create/create.php
@@ -4,19 +4,22 @@
 		<div class='border-create1'>
 			<p><?php echo $lang_ket_daftar_personal; ?></p>
 			<div class='my-btn-create'><?php echo anchor('customer/personal/', "<br/><br/><i class='fa fa-users fa-3x' aria-hidden='true'></i> ", array('class'=>''));?></div>
-		</div>
+      <p> Personal </p>
+    </div>
 	  </div>
 	  <div class="col-sm-4">
 		<div class='border-create1'>
 			<p><?php echo $lang_ket_daftar_akunBisnis;?></p>
 			<div class='my-btn-create'><?php echo anchor('customer/business/', "<i class='fa fa-briefcase fa-3x' aria-hidden='true'></i> ", array('class'=>''));?> </div>
-		</div>
+        <p> Business </p>
+    </div>
 	  </div>
 	  <div class="col-sm-4">
 		<div class='border-create1'>
-			<p>Untuk pemerintah yang ingin belanja di myacico.com.</p>
+			<p>Untuk pemerintah yang ingin belanja di myAcico.co.id</p>
 			<div class='my-btn-create'><?php echo anchor('customer/government/', "<br/><br/><i class='fa fa-building fa-3x' aria-hidden='true'></i> ", array('class'=>''));?></div>
-		</div>
+      <p> Government </p>
+    </div>
 	  </div>
 	</div>
 </div>
