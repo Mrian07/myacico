599b10bc855e66554fc654a3343bf1892e361578
obbie <wahyu.suit@myacico.com>
2017-11-20 11:35:13 +0700

    test

--- application/views/frontend/modules/product/sidebar_left.php
+++ application/views/frontend/modules/product/sidebar_left.php
@@ -1,20 +1,20 @@
 <div class='sidebar-left-mobile'>
-	<button class="btn btn-danger btn-block" data-toggle="collapse" data-target="#demo">Filter</button>
-	<div id="demo" class="collapse">
-		<?php 
-		$this->data['pane'] = "pane"; 
-		$this->data['sliderprice'] = "slider-price"; 
-		$this->data['sliderpricelow'] = "slider-price-low"; 
-		$this->data['sliderpricehigh'] = "slider-price-high"; 
+	<button class="btn btn-danger btn-block" data-toggle="collapse" data-target="#demo123">Filter</button>
+	<div id="demo123" class="collapse">
+		<?php
+		$this->data['pane'] = "pane";
+		$this->data['sliderprice'] = "slider-price";
+		$this->data['sliderpricelow'] = "slider-price-low";
+		$this->data['sliderpricehigh'] = "slider-price-high";
 		$this->load->view('frontend/modules/product/sidebar_left_filter',$this->data); ?>
-	</div>	
-</div>	
+	</div>
+</div>
 <div class='sidebar-left-desktop'>
-	<?php 
-	$this->data['pane'] = "pane2"; 
-	$this->data['sliderprice'] = "slider-price2"; 
-	$this->data['sliderpricelow'] = "slider-price-low2"; 
-	$this->data['sliderpricehigh'] = "slider-price-high2"; 
-		
+	<?php
+	$this->data['pane'] = "pane2";
+	$this->data['sliderprice'] = "slider-price2";
+	$this->data['sliderpricelow'] = "slider-price-low2";
+	$this->data['sliderpricehigh'] = "slider-price-high2";
+
 	$this->load->view('frontend/modules/product/sidebar_left_filter',$this->data); ?>
-</div>	
+</div>
