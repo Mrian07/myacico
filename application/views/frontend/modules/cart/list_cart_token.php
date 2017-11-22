03d59f7407158ffb309b1c8ecc78a258559132c0
unknown <lalang gumirang>
2017-11-22 11:07:35 +0700

    update

--- application/views/frontend/modules/cart/list_cart_token.php
+++ application/views/frontend/modules/cart/list_cart_token.php
@@ -3,7 +3,13 @@
 	$total = 0;
 	foreach($hasil as $items):
 	echo"<div class='row my-cart'>
-	  <div class='col-sm-3'><img src='".$items['imageurl']."' border='0' height='50' width='50'></div>
+	  <div class='col-sm-3'>";
+		if($items['imageurl']){
+			echo"<img src='".$items['imageurl']."' border='0' width='50'>";
+		}else{
+			echo"<img src='".base_url('images/general/noimage.png')."' style='height:50px; width: auto' border='0'>";
+		}
+	echo"</div>
 	  <div class='col-sm-7'><b>".$items['name']."</b><br>Rp.".money($items['price'])." <i>(".$items['qty'].")Items</i><br></div>
 	  <div class='col-sm-2'><a href='#' onClick=\"dellItemCartToken('".$items['productId']."','".$items['imageurl']."','".$items['name']."','".$items['itemCartId']."')\"><i class='fa fa-trash' aria-hidden='true'></i></a></div>
 	</div>";
@@ -11,6 +17,6 @@
 	endforeach;
 	?>
 </div>
-<div style='color:#ffffff;'>
+<div style='color:#111111; text-align:center'>
 	<div class='my-total-cart'>TOTAL : <b>Rp.<?php echo money($total); ?></b></div><?php echo anchor('cart', 'My Cart & Checkout', array('class'=>'btn btn-success','style'=>'color:#ffffff'));?>
 </div>
