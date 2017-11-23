99e9a52b4fbd3cb91964057986f9ebd412a88d4d
unknown <lalang gumirang>
2017-11-23 17:59:36 +0700

    edit password

--- application/views/frontend/modules/product/product.php
+++ application/views/frontend/modules/product/product.php
@@ -1,19 +1,19 @@
 
-	<div class="container">
-		<div class='my-bg-title'>
-
-			<i class="fa fa-angle-right" aria-hidden="true"></i> LIST PRODUCT
-
-		</div>
-	<div class="row">
-	  <div class="col-sm-2"><?php $this->load->view('frontend/modules/product/sidebar_left',$this->data); ?></div>
-	  <div class="col-sm-10"><?php
-		if($this->session->userdata('itemView')=='grid'){
-			$this->load->view('frontend/modules/product/product_items',$this->data);
-		}else{
-			$this->load->view('frontend/modules/product/product_items_list',$this->data);
-		}
-		 ?></div>
-	  <!-- <div class="col-sm-2"><?php // $this->load->view('frontend/modules/product/sidebar_right',$this->data); ?></div>-->
+<div class="container">
+	<div class='my-bg-title'>
+	<i class="fa fa-angle-right" aria-hidden="true"></i> LIST PRODUCT
 	</div>
+<div class="row">
+<div class="col-sm-2"><?php $this->load->view('frontend/modules/product/sidebar_left',$this->data); ?></div>
+<div class="col-sm-10">
+	<?php
+	if($this->session->userdata('itemView')=='grid'){
+	$this->load->view('frontend/modules/product/product_items',$this->data);
+	}else{
+	$this->load->view('frontend/modules/product/product_items_list',$this->data);
+	}
+	?>
+</div>
+<!-- <div class="col-sm-2"><?php // $this->load->view('frontend/modules/product/sidebar_right',$this->data); ?></div>-->
+</div>
 </div>
