3b5ea73104b80efa3dd30ad1e6763b89df59dd49
unknown <lalang gumirang>
2017-11-24 10:31:43 +0700

    lost password

--- application/views/frontend/modules/customer/create/registration_completed.php
+++ application/views/frontend/modules/customer/create/registration_completed.php
@@ -7,6 +7,7 @@
 				<h4>Aktivasi account anda berhasil. </h4><h5><?php echo anchor('customer/signIn','click disini untuk login.'); ?></h5>
 			<?php }else{ ?>
 				<h4>Maaf aktivasi account Anda gagal, silakan lakukan pendaftaran kembali.</h4>
+				
 			<?php } ?>
 		</div>
 	  </div>
