28914794a36752640c39f9841b27ac8bfd0c741f
unknown <lalang gumirang>
2017-12-04 14:37:39 +0700

    head

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -849,8 +849,9 @@ $(".logout").click(function(e){
 			confirm: function () {
 				document.cookie='x-auth=; path='+base_path+';expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 				<?php
-				//untuk menghapus cookie 
-				unset($_COOKIE['shipping_address_id']); ?>
+				//untuk menghapus cookie
+				set_cookie('shipping_address_id','',time() + (86400 * 30));
+				?>
 				location.href='<?php echo base_url("customer/signIn");?>';
 				if(chat.soc){
 					chat.soc.disconnect();
