381eef8988b097ab6e236c97c3bad4569a5a09b4
obbie <wahyu.suit@myacico.com>
2017-12-04 10:04:33 +0700

    konfrimasi penambahaan

--- application/views/frontend/modules/pages/konfrimasi.php
+++ application/views/frontend/modules/pages/konfrimasi.php
@@ -22,7 +22,12 @@ font-size: 14px;
       <center>
 <button type="button" class="btn btn-primary test" name="button" disabled>Konfrimasi Order </button>
 <hr>
-<span class="testKuh" align="left"> Jika order kamu telah di konfrimasi, kamu akan mendapatkan email konfrimasi order di email yang kamu gunakan </span>
+<span class="testKuh" align="left"> Jika order kamu telah di konfrimasi, kamu akan mendapatkan email konfrimasi order di email yang kamu gunakan. </span><br/><br/>
+<span class="testKuh" align="left"> Email Konfrimasi Order Akan Terdiri Dari : </span> <br/>
+<span class="testKuh" align="left"> 1. Nomor Order </span><br/>
+<span class="testKuh" align="left"> 2. Jumlah Harga </span><br/>
+<span class="testKuh" align="left"> 3. Jumlah Pesanan </span><br/>
+<span class="testKuh" align="left"> 4. Alamat Penagihan Dan Pengiriman </span>
 </center>
 
 </div>
