8fcd16ab31db326bfdd9fc043306e761c7231cfd
samuel <samuel.adhirinaldi@live.com>
2017-12-05 16:28:25 +0700

    menu bar

--- application/views/frontend/nav.php
+++ application/views/frontend/nav.php
@@ -47,14 +47,14 @@ cursor:pointer;
 
 	<div class='my-nav'>
 <!-- Start Navigation -->
-    <nav class="navbar navbar-default navbar-custom bootsnav" style='width100%; margin: 0 auto;'>
+    <nav class="navbar navbar-default navbar-custom bootsnav" style='width:100%;'>
 			<!-- Start Header Navigation -->
             <!-- End Header Navigation -->
 
             <!-- Collect the nav links, forms, and other content for toggling -->
 
             <div class="collapse navbar-collapse" id="navbar-menu">
-                <ul class="nav navbar-nav navbar-center" data-in="fadeInDown" data-out="fadeOutUp" style='text-align:center'>
+                <ul class="nav navbar-nav navbar-center" style="margin-right:7%; margin-left: 7%;" data-in="fadeInDown" data-out="fadeOutUp" style='text-align:center'>
 
 
 									<?php
@@ -69,7 +69,7 @@ cursor:pointer;
                      ?>
 									<li class="dropdown megamenu-fw nav-show-desktop nav-garis">
 										<?php echo anchor('#', '<img src="'.$dataNav['iconurl'].'" border="0" height="20"> '.$nav_name, array('class'=>'dropdown-toggle','data-toggle'=>'dropdown'));?>
-										<ul class="dropdown-menu megamenu-content" role="menu">
+                                                                            <ul  class="dropdown-menu megamenu-content" role="menu">
 											<li>
 												<div>
 													<div class="row" style='color:#000000'>
