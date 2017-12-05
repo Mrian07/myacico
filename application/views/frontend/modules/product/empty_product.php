eda80c4f16a7629d4d34225bea8dc8e7ea0c50da
unknown <lalang gumirang>
2017-12-05 17:11:05 +0700

    produk kosong akan dialihkan

---
+++ application/views/frontend/modules/product/empty_product.php
@@ -0,0 +1,10 @@
+
+<div class="container">
+    <div class="row">
+	  <div class="col-sm-12"><br>
+		<div class='border-create'>
+			Maaf produk yang anda cari tidak ditemukan.
+		</div>
+	  </div>
+	</div>
+</div>
