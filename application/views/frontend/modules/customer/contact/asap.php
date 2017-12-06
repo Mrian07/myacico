d004327b5c23d6ba8be574fdc0ec0be2af03d970
samuel <samuel.adhirinaldi@live.com>
2017-12-06 17:15:25 +0700

    asap big pic

--- application/views/frontend/modules/customer/contact/asap.php
+++ application/views/frontend/modules/customer/contact/asap.php
@@ -3,6 +3,9 @@
 	  <div class="col-sm-12">
               <img src="https://storage.googleapis.com/myacico/image/banner/banner-58.png" style="width:100%; ">
 		<br>
+                <center>
+                    <img src="<?php echo base_url('images/general/asap_big.png');?>" style='height:auto; width: auto;'>
+                </center>
 		<?php echo $lang_inf_asap;?>
 		
 	  </div>
