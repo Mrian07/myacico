4febaf59e4940bff21c628edbc46f6f34ddb8a76
unknown <lalang gumirang>
2017-12-07 12:42:22 +0700

    logo dan test di custom

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -252,7 +252,7 @@ var prod_detail =  localStorage.getItem('product_detail');
 
 
 		 	<div class='my-hdr1'>
-				<div style='margin-top:-3px'>
+				<div style='margin-top:-10px; margin-left:20px'>
 					<?php echo anchor('/', '<img src="'.base_url('images/general/logocoid.gif').'" border="0" height="70">');?>
 				</div>
 			</div>
