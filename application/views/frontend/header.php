c5a29e1d1d5b5fc1e7e9b1300f62e2480adf204f
unknown <lalang gumirang>
2017-12-07 14:28:42 +0700

    banner atas

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -108,7 +108,7 @@ var prod_detail =  localStorage.getItem('product_detail');
     </head>
 
 	<body style='font-weight: normal;' ng-app="myApp">
-			<div class="top-bg" onclick="location.href='<?php echo base_url('product/category/1000540/Peralatan-Dapur');?>'" style='cursor: pointer;'><img src='<?php echo base_url('images/general/bg-top_main.jpg');?>' border='0'></div>
+			<div class="top-bg" onclick="location.href='<?php echo base_url('product/category/1000540/Peralatan-Dapur');?>'" style='cursor: pointer;'><img src='<?php echo base_url('images/general/bg-top_main.png');?>' border='0'></div>
 			<div class='row'>
 				<!-- <div class="col-xs-7" style='display: flex;  align-items: flex-end;height: 60px;border: solid 0px red; text-align:right; background:#dddddd;'> -->
 
