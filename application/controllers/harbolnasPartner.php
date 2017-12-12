d04fe288eed2b486b8e97e5cf6e156860c3b1637
obbie <wahyu.suit@myacico.com>
2017-12-12 10:14:43 +0700

    update

--- application/controllers/harbolnasPartner.php
+++ application/controllers/harbolnasPartner.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Harbolnaspartner extends Web {
+class HarbolnasPartner extends Web {
 
 	public function __construct()
     {
