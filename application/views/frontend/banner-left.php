8c618266effcc661fd826de2e0b9eef6c2dfec1d
obbie <wahyu.suit@myacico.com>
2017-12-13 09:47:35 +0700

    styling Semua CSS

--- application/views/frontend/banner-left.php
+++ application/views/frontend/banner-left.php
@@ -1,58 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-#banner-left {
-    position: fixed;
-    left: 0;
-    top: 300px;
-    transition: 0.3s;
-    display: none;
-}
-#banner-left img {
-    width: 140px;
-}
-
-@media only screen and (min-width: 1440px) {
-    #banner-left {
-        display: block;
-    }
-
-    #banner-left img {
-        width: 140px;
-    }
-}
-
-@media only screen and (min-width: 1600px) {
-    #banner-left {
-        display: block;
-    }
-
-    #banner-left img {
-        width: 210px;
-    }
-}
-
-@media only screen and (min-width: 2048px) {
-    #banner-left {
-        display: block;
-    }
-
-    #banner-left img {
-        width: 400px;
-    }
-}
-
-@media only screen and (min-width: 2560px) {
-    #banner-left {
-        display: block;
-    }
-
-    #banner-left img {
-        width: 440px;
-    }
-}
-</style>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style_banner_left.css');?>" />
 </head>
 <body>
 
