433d1c656f98337087e23728c9f34a6b838930be
obbie <wahyu.suit@myacico.com>
2018-01-05 16:15:58 +0700

    Faq Dan After Sales

--- application/views/frontend/modules/pages/faq.php
+++ application/views/frontend/modules/pages/faq.php
@@ -1,80 +1,67 @@
-<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style_faq.css');?>" />
 <div class="container">
-<div class="row bootstrap snippets">
-    <div class="col-md-4 col-sm-6 content-card">
-        <div class="card-big-shadow">
-            <div class="card card-just-text" data-background="color" data-color="blue" data-radius="none">
-                <div class="content">
-                  <i class="fa fa-user-o fa-5x"></i>
-                    <h4 class="title"><?php echo anchor('Pages/AboutUs',$lang_tentangkami); ?></h4>
-                    <!-- <p class="description">What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment. </p> -->
-                </div>
-            </div> <!-- end card -->
-        </div>
-    </div>
-
-    <div class="col-md-4 col-sm-6 content-card">
-        <div class="card-big-shadow">
-            <div class="card card-just-text" data-background="color" data-color="green" data-radius="none">
-                <div class="content">
-                      <i class="fa fa-gift fa-5x"></i>
-                    <h4 class="title"><?php echo anchor('Pages/faqCus',$lang_f_balik); ?></h4>
-                    <!-- <p class="description">What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment. </p> -->
-                </div>
-            </div> <!-- end card -->
-        </div>
-    </div>
-
-    <div class="col-md-4 col-sm-6 content-card">
-        <div class="card-big-shadow">
-            <div class="card card-just-text" data-background="color" data-color="yellow" data-radius="none">
-                <div class="content">
-                  <i class="fa fa-money fa-5x"></i>
-                <h4 class="title"><?php echo anchor('Pages/konfrimasi',$lang_btn_confirm); ?></h4>
-                    <!-- <p class="description">What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment. </p> -->
-                </div>
-            </div> <!-- end card -->
-        </div>
-    </div>
-
-    <!-- <div class="col-md-4 col-sm-6 content-card">
-        <div class="card-big-shadow">
-            <div class="card card-just-text" data-background="color" data-color="brown" data-radius="none">
-                <div class="content">
-                  <i class="fa fa-heart fa-5x"></i>
-                      <h4 class="title"><?php // echo anchor('Pages/Pemb',$lang_guara); ?></h4>
-
-                </div>
+  <h2>Collapsible Panel</h2>
+  <p>Click on the collapsible panel to open and close it.</p>
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" href="#collapse1">Bagaimana cara melakukan pemesanan di myACICO.co.id?</a>
+        </h4>
+      </div>
+      <div id="collapse1" class="panel-collapse collapse">
+        <div class="panel-body">Cara melakukan pemesanan di myACICO adalah sebagai berikut</div>
+        <ul>
+          <li>Pilih produk yang ingin dibeli</li>
+          <li>Klik <b>Beli Sekarang</b></li>
+          <li>Klik Masukkan alamat pengiriman dan pilih jasa pengiriman yang ingin digunakan</li>
+          <li>Klik <b>Bayar Sekarang</b></li>
+          <li>Klik <b>Mulai Checkout</b></li>
+        </ul>
+      </div>
+      <hr>
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" href="#collapse2">Bagaimana cara membuat akun di myACICO.co.id?</a>
+        </h4>
+      </div>
+            <div id="collapse2" class="panel-collapse collapse">
+            <div class="panel-body">Untuk membuat akun personal, silakan mengikuti langkah-langkah berikut</div>
+            <ul>
+              <li>Klik Daftar</li>
+              <li>Isi kolom Nama, Email, Password dan Ulangi Password</li>
+              <li>Selanjutnya myACICO akan mengirimankan email verifikasi ke alamat email yang didaftarkan</li>
+              <li>Silakan klik Activate pada email verifikasi yang telah dikirimkan</b></li>
+            </ul>
             </div>
-        </div>
-    </div> -->
+            <hr>
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" href="#collapse3">Apakah saya bisa melakukan pengambilan barang secara langsung ke gudang myACICO?</a>
+              </h4>
+            </div>
+            <div id="collapse3" class="panel-collapse collapse">
+            <div class="panel-body">Mohon maaf, saat ini belum dapat dilakukan pengambilan secara langsung. Pesanan akan dikirimkan oleh jasa pengiriman yang telah dipilih ke alamat tujuan.</div>
+            </div>
+            <hr>
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" href="#collapse4">Apakah saya dapat melakukan pembayaran dengan cara COD (Cash On Delivery)?</a>
+              </h4>
+            </div>
+            <div id="collapse4" class="panel-collapse collapse">
+            <div class="panel-body">Untuk saat ini belum tersedia pembayaran dengan metode COD, silakan memilih metode pembayaran yang tersedia (Transfer Bank dan Kartu Kredit)</div>
+            </div>
+            <hr>
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" href="#collapse5">Bagaimana cara saya membatalkan pesanan?</a>
+              </h4>
+            </div>
+            <div id="collapse5" class="panel-collapse collapse">
+            <div class="panel-body">Silakan hubungi Customer Service kami di 021 3502099 & 021 3453533 atau melalui Live Chat pada website Myacico.co.id agar dapat dibantu untuk proses pembatalan. Namun pesanan yang dapat dibatalkan hanya untuk pesanan yang belum dilakukan pengiriman, dengan kata lain pesanan tidak dapat dibatalkan apabila status pesanan telah dikirim.</div>
+            </div>
 
-    <div class="col-md-4 col-sm-6 content-card">
-        <div class="card-big-shadow">
-            <div class="card card-just-text" data-background="color" data-color="purple" data-radius="none">
-                <div class="content">
-                            <i class="fa fa-phone fa-5x"></i>
-                    <h4 class="title"><?php echo anchor('Customer/contact','CallUs'); ?></h4>
-                    <!-- <p class="description">What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment. </p> -->
-                </div>
-            </div> <!-- end card -->
-        </div>
-    </div>
 
-    <div class="col-md-4 col-sm-6 content-card">
-        <div class="card-big-shadow">
-            <div class="card card-just-text" data-background="color" data-color="orange" data-radius="none">
-                <div class="content">
-                    <i class="fa fa-ban fa-5x"></i>
-                    <h4 class="title"><a href="#">Cancel</a></h4>
-                    <!-- <p class="description">What all of these have in common is that they're pulling information out of the app or the service and making it relevant to the moment. </p> -->
-                </div>
-            </div> <!-- end card -->
-        </div>
     </div>
+  </div>
 </div>
-</div>
-<br>
-<br>
-<br>
-<br>
