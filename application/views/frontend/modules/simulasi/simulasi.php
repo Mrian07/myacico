be03601cf34a4a1e77c725ee80cbede585c48eeb
unknown <lalang gumirang>
2018-03-14 14:45:33 +0700

    simulasi jasa pengiriman

---
+++ application/views/frontend/modules/simulasi/simulasi.php
@@ -0,0 +1,79 @@
+<div class="container">
+  <div class="b-simulasi">
+    <div class="title-simulasi">SIMULASI TARIF PENGIRIMAN</div>
+    <div class="sub-title-simulasi">Simulasi Tarif Pengiriman ke:</div>
+    <p><b>JENIS EKSPEDISI</b></p>
+    <table border="0" width="100%">
+      <tr>
+        <td width="50%">Propinsi</td>
+        <td>
+          <select class="form-control">
+            <option>Aceh</option>
+          </select>
+        </td>
+      </tr><tr>  
+        <td width="50%">Kota</td><td>
+          <select class="form-control">
+            <option>Bener Meriah</option>
+          </select>
+        </td>
+      </tr><tr>    
+        <td width="50%">Kecamatan</td><td>
+          <select class="form-control">
+            <option>-Pilih Kecamatan-</option>
+          </select>
+        </td>
+      </tr><tr>  
+        <td width="50%">Kode Pos</td><td>
+          <select class="form-control">
+            <option>-Pilih Kode Pos-</option>
+          </select>
+        </td>
+      </tr><tr>  
+        <td width="50%">Jasa Pengiriman</td><td>
+          <select class="form-control">
+            <option>-Pilih Jasa Pengiriman-</option>
+          </select>
+        </td>
+      </tr>
+    </table>
+
+    <div class="sub-title-simulasi">Simulasi Harga Pengiriman</div>
+    <table border="0" width="100%">
+      <tr>
+        <td width="50%">Harga Produk <span class="small-text-simulasi">(Harga satuan produk yang dibeli)</span></td>
+        <td>
+          <div class="form-group row">
+            <div class="col-xs-6">
+              <div class="input-group">
+                <span class="input-group-addon">Rp.</span>
+                <input type="text" class="form-control" name="email">
+              </div>
+            </div>
+          </div>
+        </td>
+      </tr>
+      <tr>  
+        <td width="50%">Kuantiti Produk <span class="small-text-simulasi">(Kuantiti produk yang dibeli)</span></td>
+        <td>
+          <div class="row">
+            <div class="col-xs-2">
+              <input type="text" class="form-control">
+            </div>
+          </div>
+        </td>
+      </tr><tr>    
+        <td width="50%">Ongkos Kirim <span class="small-text-simulasi">(Terima asuransi)</span></td><td>
+          <b>Rp.</b>
+        </td>
+      </tr><tr>  
+        <td width="50%">Total Biaya <span class="small-text-simulasi">(Total harga produk + ongkos kirim)</span></td><td>
+          <b>Rp.</b>
+        </td>
+      </tr>
+    </table>
+
+    <div class="title-simulasi">SIMULASI TARIF PENGIRIMAN</div>
+
+  </div>
+</div>
