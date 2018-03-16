9416cd5f24a650f5ef8c0d6e76d085c90d9bd144
unknown <lalang gumirang>
2018-03-16 13:59:53 +0700

    test env

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,60 +10,32 @@ class Home extends Web {
 		$this->load->library('form_validation');
 		$this->load->library('session');
 		$this->load->helper('app');
-	//	$this->load->helper('cookie');
-	//	$this->load->model('Login_model', 'login', TRUE);
-
-
-		$this->atribut();
-	}
-
-	public function atribut(){
-
 	}
 
 	public function maintenance()
-  {
+  	{
 		$this->load->view('frontend/maintenance.php');
 	}
 
 	public function index()
     {
-            $home_domain = domain2();
-		//Slide slide_show
+        $home_domain = domain2();
 		$api = "home/view";
-                $api_lvl1 ="product/banner";
-                $url_lvl1 =api_base_url($api_lvl1);
+		$api_lvl1 ="product/banner";
+		$url_lvl1 =api_base_url($api_lvl1);
 
 		$url = api_base_url($api);
 
 		$api2 = "home/topproductcategory";
 		$url2 = api_base_url($api2);
 		$konten2 = file_get_contents($url2);
-		// echo $url2;
-		// die();
+
 		$konten = file_get_contents($url);
-                $konten_lvl1 = file_get_contents($url_lvl1);
+        $konten_lvl1 = file_get_contents($url_lvl1);
 		$this->data['dathome'] = json_decode($konten, true);
 		$this->data['dathome2'] = json_decode($konten2, true);
-                $this->data['dathomeLvl'] = json_decode($konten_lvl1, true);
+        $this->data['dathomeLvl'] = json_decode($konten_lvl1, true);
 
-		 /*echo "<pre>";
-		 print_r($this->data['dathomeLvl'] );
-		 die();*/
-		//$dathome2 = json_decode($konten2, true);
-		//$this->data['dathome3'] = $dathome2['topProduct'][0]['topProduct']
-
-// 				echo "<pre>";
-// 				print_r ($dathome2['topProduct'][0]['topProduct']);
-// // print_r($dathome2);
-// die();
-	// 	echo "<pre>";
-	// 	print_r($dathome2['topProduct']);
-	// //	print_r($dathome2[0]['category']);
-	// echo "</pre>";
-	// 	die();
-
-		//$this->load->view('frontend/test',$this->data);
 		$this->data['title_web'] = "myacico.co.id: ".$home_domain;
 		$this->load->view('frontend/header',$this->data);
 		$this->load->view('frontend/nav.php',$this->data);
@@ -80,31 +52,25 @@ class Home extends Web {
 		set_cookie('lang',$lang,'3600');
 		echo $lang;
 	}
-	/*
-	public function index() {
-         set_cookie('cookie_name','cookie_value','3600');
-         $this->load->view('Cookie_view');
-      }
-  */
-      public function display_cookie() {
-         echo get_cookie('lang');
-      //   $this->load->view('Cookie_view');
-      }
 
-      public function deletecookie() {
-         delete_cookie('lang');
-     //    redirect('cookie/display');
-      }
+	public function display_cookie() {
+		echo get_cookie('lang');
+	}
 
-			public function testserver() {
-				echo"testing:<p>";
-				echo getenv("SRV_ENV");
-				if(getenv("SRV_ENV")=="prod"){
-				  $url = "https://api.myacico.co.id/myacico-service/";
-					echo $url;
-				}else{
-				  $url = "https://api.myacico.co.id/dev/";
-					echo $url;
-				}
-			}
+	public function deletecookie() {
+		delete_cookie('lang');
+	}
+
+	public function testserver() {
+		echo"Mengarah ke server: ";
+		echo getenv("SRV_ENV");
+		echo"<p>";
+		if(getenv("SRV_ENV")=="prod"){
+			$url = "https://api.myacico.co.id/myacico-service/";
+			echo $url;
+		}else{
+			$url = "https://api.myacico.co.id/dev/";
+			echo $url;
+		}
+	}
 }
